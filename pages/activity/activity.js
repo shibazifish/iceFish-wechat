6a38c9c9957af593c8072ee5ec84da6f038caa9e
李宇 <623763070@qq.com>
2019-01-03 14:45:17 +0800

    清除冗余代码

--- pages/activity/activity.js
+++ pages/activity/activity.js
@@ -9,17 +9,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    goodsInfo: [],
-    inviter: '',
-    notice: '每周三补充库存',
-    marqueePace: 1,//滚动速度
-    marqueeDistance: 0,//初始滚动距离
-    marqueeDistance2: 0,
-    marquee2copy_status: false,
-    marquee2_margin: 60,
-    size: 14,
-    orientation: 'left',//滚动方向
-    interval: 20 // 时间间隔
+    activityInfo: [],
   },
 
   /**
@@ -27,28 +17,15 @@ Page({
    */
   onLoad: function (options) {
     var that = this;
-    if (options.inviter != undefined) {
-      that.setData({
-        inviter: options.inviter,
-      })
-      console.log("inviter:" + options.inviter);
-    }
     wx.showLoading({
       title: '数据加载中...',
     })
-    this.getGoodsInfo();
-    setTimeout(function () {
-      if (app.globalData.nickname == '') {
-        wx.navigateTo({
-          url: '/pages/grant/grant?inviter=' + that.data.inviter,
-        })
-      }
-    }, 4000);
+    that.getActivityInfo();
   },
   /**
-   * 获取奖品信息 2018年11月27日20:11:24
+   * 获取活动信息 2018年11月27日20:11:24
    */
-  getGoodsInfo: function () {
+  getActivityInfo: function () {
     let that = this;
     util.request(api.GoodsInfoUrl).then(function (res) {
       wx.hideLoading();
@@ -112,9 +89,9 @@ Page({
    */
   onShareAppMessage: function () {
     return {
-      title: '每天走路就能兑礼品啦！',
-      imageUrl: '../../static/images/hongbao.jpg',
-      path: '/pages/prize/prize?inviter=' + app.globalData.openid,// 用户点击首先进入的当前页面
+      title: '多重活动，等你来参加！',
+      imageUrl: '/static/images/zan.png',
+      path: '/pages/activity/activity',// 用户点击首先进入的当前页面
       success: function (res) {
         // 转发成功
         console.log("转发成功:");
@@ -133,59 +110,4 @@ Page({
       url: '/pages/activity/activityEdit',
     })
   },
-  onShow: function () {
-    // 页面显示
-    var vm = this;
-    var length = vm.data.notice.length * vm.data.size;//文字长度
-    var windowWidth = wx.getSystemInfoSync().windowWidth;// 屏幕宽度
-    vm.setData({
-      length: length,
-      windowWidth: windowWidth,
-      marquee2_margin: length < windowWidth ? windowWidth - length : vm.data.marquee2_margin//当文字长度小于屏幕长度时，需要增加补白
-    });
-    vm.run1();// 水平一行字滚动完了再按照原来的方向滚动
-    vm.run2();// 第一个字消失后立即从右边出现
-  },
-  run1: function () {
-    var vm = this;
-    var interval = setInterval(function () {
-      if (-vm.data.marqueeDistance < vm.data.length) {
-        vm.setData({
-          marqueeDistance: vm.data.marqueeDistance - vm.data.marqueePace,
-        });
-      } else {
-        clearInterval(interval);
-        vm.setData({
-          marqueeDistance: vm.data.windowWidth
-        });
-        vm.run1();
-      }
-    }, vm.data.interval);
-  },
-  run2: function () {
-    var vm = this;
-    var interval = setInterval(function () {
-      if (-vm.data.marqueeDistance2 < vm.data.length) {
-        // 如果文字滚动到出现marquee2_margin=30px的白边，就接着显示
-        vm.setData({
-          marqueeDistance2: vm.data.marqueeDistance2 - vm.data.marqueePace,
-          marquee2copy_status: vm.data.length + vm.data.marqueeDistance2 <= vm.data.windowWidth + vm.data.marquee2_margin,
-        });
-      } else {
-        if (-vm.data.marqueeDistance2 >= vm.data.marquee2_margin) { // 当第二条文字滚动到最左边时
-          vm.setData({
-            marqueeDistance2: vm.data.marquee2_margin // 直接重新滚动
-          });
-          clearInterval(interval);
-          vm.run2();
-        } else {
-          clearInterval(interval);
-          vm.setData({
-            marqueeDistance2: -vm.data.windowWidth
-          });
-          vm.run2();
-        }
-      }
-    }, vm.data.interval);
-  }
 })
